69900048d5c77b13ad6a8715ce68f6129c4911a7
Matt Keys <MatthewjKeys22@gmail.com>
2022-02-04 19:32:38 -0700

    some typedefs and resolvers declared, working on mutations and queries for client side next

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,42 @@
+// import the gql tagged template function
+const { gql } = require('apollo-server-express');
+
+// create our typeDefs
+const typeDefs = gql`
+
+  type Book {
+    _id: ID
+    authors: [{String}]
+    description: String!
+    bookId: String!
+    image: String
+    link: String
+    title: String!
+  }
+
+  type User {
+    _id: ID
+    username: String
+    email: String
+    savedBooks: [Book]
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    me: User
+    users: [User]
+    user(username: String!): User
+    book: Book
+  }
+
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
